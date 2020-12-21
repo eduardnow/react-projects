95ab329d81adf1b7c8a956663fae2751bd498934
Eduard Castro <eduard.cas07@gmail.com>
2020-12-21 12:23:58 -0500

    [start learning react] Add Hello World with display output example

--- start-learning-react/src/HelloWorld.js
+++ start-learning-react/src/HelloWorld.js
@@ -2,7 +2,15 @@ import React from "react";
 
 class HelloWorld extends React.Component {
     render() {
-        return <h1>Hello World!</h1>
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, earum eius eveniet ipsam itaque modi nostrum,
+                    odit quis, veniam?
+                </p>
+            </div>
+        )
     }
 }
 
