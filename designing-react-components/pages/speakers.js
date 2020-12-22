c0bb7cc44620f7cea9d4091b294e43cb0c9270f1
Eduard Castro <eduard.cas07@gmail.com>
2020-12-22 08:56:58 -0500

    [designing-react-component] Add speakers page

---
+++ designing-react-components/pages/speakers.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Page extends React.Component {
+    render() {
+        return (
+           <div>
+               <img src="images/header.png" alt=""/>
+               <img src="images/menu.gif" alt=""/>
+               <img src="images/searchbar.gif" alt=""/>
+               <img src="images/speakers.png" alt=""/>
+               <img src="images/footer.png" alt=""/>
+           </div>
+        );
+    }
+}
+
+export default Page
