dcdd810831478b97c9a9eb8aea1461e1c09dbb6a
Eduard Castro <eduard.cas07@gmail.com>
2020-12-22 11:40:32 -0500

    [designing-react-component] Add speakers component

--- designing-react-components/src/components/Speakers.js
+++ designing-react-components/src/components/Speakers.js
@@ -1,3 +1,28 @@
-const Speakers = () => <img src="images/speakers.png"  alt="header"/>;
+const Speakers = () => {
+
+    const speakers = [
+        {
+            name: 'Douglas Crockford',
+            src: 'speaker-component-1124',
+        },
+        {
+            name: 'Tamara Baker',
+            src: 'speaker-component-1530',
+        },
+        {
+            name: 'Eugene chuvyron',
+            src: 'speaker-component-10803',
+        }
+    ];
+
+    return (
+        <div>
+            {speakers.map(({name, src}) => {
+                return <img src={`/images/${src}.png`} alt={name} key={src}/>;
+            })}
+        </div>
+    );
+
+};
 
 export default Speakers
